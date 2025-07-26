c34721c62f88134abe5b9bb837ba9ce407a3f4d3
Gregory Wagner <wagner.greg@gmail.com>
2025-07-26 08:53:25 -0800

    First try

---
+++ rectilinear_land.jl
@@ -0,0 +1,48 @@
+using Oceananigans
+using Oceananigans.Utils: launch!
+using Oceananigans.Operators: ∂²zᵃᵃᶜ, Δzᵃᵃᶜ
+using Oceananigans.BoundaryConditions: fill_halo_regions!
+using KernelAbstractions: @kernel, @index
+
+@kernel function _step_temperature!(T, grid, κ, Q, Δt)
+    i, j, k = @index(Global, NTuple)
+
+    # Interior heat flux
+    dTdt = κ * ∂²zᵃᵃᶜ(i, j, k, grid, T)
+
+    # add surface contribution
+    Qk = @inbounds Q[i, j, k]
+    Δz = Δzᵃᵃᶜ(i, j, k, grid)
+    dTdt += ifelse(k == grid.Nz, - Qk / Δz, zero(grid))
+    
+    @inbounds T[i, j, k] += Δt * dTdt
+end
+
+function step_temperature!(T, κ, Q, Δt)
+    fill_halo_regions!(T)
+    grid = T.grid
+    arch = grid.architecture
+    launch!(arch, grid, :xyz, _step_temperature!, T, grid, κ, Q, Δt)
+    return nothing
+end
+
+arch = CPU()
+
+# x will be the horizontal dimension
+Nh = 100
+Nz = 10
+Lz = 10
+
+grid = RectilinearGrid(arch, size=(Nh, Nz), x=(0, 1), z=(-Lz, 0), topology=(Periodic, Flat, Bounded))
+Q = Field{Center, Center, Nothing}(grid) # surface heat flux
+T = CenterField(grid) # temperature
+
+set!(Q, 10) # modest cooling
+set!(T, 20)
+
+Δt = 0.1
+κ = 1 # m² / s
+
+for n = 1:100
+    step_temperature!(T, κ, Q, Δt)
+end
